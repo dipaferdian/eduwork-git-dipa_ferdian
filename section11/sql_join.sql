04a5450dc60095bdaa0b79ab81ed2703d8c0a66b
dipaferdian <dipaferdian2@gmail.com>
2022-12-20 21:00:46 +0700

    revisi sql join

--- section11/sql_join.sql
+++ section11/sql_join.sql
@@ -49,3 +49,33 @@ create table products(
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (category_id) REFERENCES categories(id)
 );
+
+
+INSERT INTO users(email, name, password, role, status, created_at, updated_at)
+
+VALUES ('dipaferdian2@gmail.com', 'dipaferdian', 'admin', 'admin', 'active', NOW(), NOW());
+
+INSERT INTO users(email, name, password, role, status, created_at, updated_at)
+
+VALUES ('syeikh@gmail.com', 'syeikh', 'admin', 'admin', 'active', NOW(), NOW());
+
+SET @user = (select id from users limit 1);
+
+INSERT INTO categories(user_id, code, name, slug, description, status, photo)
+
+VALUES (@user, 'A1', 'CAR', 'car_electric', 'New Car', 'active', 'http://avanza.png');
+
+SET @category = (select id from categories limit 1);
+
+INSERT INTO products(category_id, user_id, code, name, slug, description, photo, qty, unit, price, status)
+
+VALUES (@category, @user, 'A1', 'AVANZA', 'avanza_electric', 'NEW Avanza car', 'http://avanza.png', 1, '1', 1000.0000, 'available');
+
+INSERT INTO products(category_id, user_id, code, name, slug, description, photo, qty, unit, price, status)
+
+VALUES (@category, @user, 'T1', 'Tesla', 'tesla_electric', 'NEW Tesla car', 'http://tesla.png', 1, '1', 2000.0000, 'available')
+
+
+SELECT users.id, users.name, c.name as categories, p.name products FROM users
+LEFT JOIN categories c on users.id = c.user_id
+LEFT JOIN products p on c.id = p.category_id and users.id = p.user_id
